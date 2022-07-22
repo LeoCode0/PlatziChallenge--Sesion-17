ab5431ae23c667ce40e474f44bd583f851f711fa
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-07-22 09:54:38 -0500

    fix netlify

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker } from 'react-leaflet'
 
 import "leaflet/dist/leaflet.css"
 
